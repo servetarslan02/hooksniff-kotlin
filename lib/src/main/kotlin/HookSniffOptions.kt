f62f45316f4c4d92fcc8d1b22f62ca1a76296e5a
servetarslan02 <servetarslan02@gmail.com>
2026-05-19 03:04:08 +0800

    feat: timeoutMs, debug, headers added to HookSniffOptions

--- lib/src/main/kotlin/HookSniffOptions.kt
+++ lib/src/main/kotlin/HookSniffOptions.kt
@@ -3,10 +3,18 @@ package com.hooksniff.kotlin
 data class HookSniffOptions(
     var baseUrl: String? = null,
     val retrySchedule: List<Long> = listOf(1000, 2000, 4000),
+    var timeoutMs: Long = 30000,
+    var debug: Boolean = false,
+    var headers: MutableMap<String, String> = mutableMapOf()
 ) {
     init {
         if (retrySchedule.count() > 5) {
             throw IllegalArgumentException("number of retries must not exceed 5")
         }
+    }
+
+    fun header(name: String, value: String): HookSniffOptions {
+        headers[name] = value
+        return this
     }
 }
